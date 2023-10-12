f82add18940b182d0113358f603e1caf71e4fe1a
Ahmer <muhammadahmer.akram@ceative.co.uk>
2023-10-12 21:51:40 +0500

    react hook form

--- src/components/react-hook-form/RHFDatePicker.tsx
+++ src/components/react-hook-form/RHFDatePicker.tsx
@@ -1,11 +1,10 @@
-// form
 import { useFormContext, Controller } from "react-hook-form";
-// @mui
 import { DatePicker } from "@mui/x-date-pickers";
-import { Typography } from "@mui/material";
+import { Typography, useTheme } from "@mui/material";
 import CustomLabel from "./Label";
-
-// ----------------------------------------------------------------------
+import ErrorIcon from "@mui/icons-material/Error";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
 export default function RHFDatePicker({
   name,
@@ -13,40 +12,52 @@ export default function RHFDatePicker({
   required,
   ...other
 }: any) {
+  const theme: any = useTheme();
+
   const { control } = useFormContext();
+
   return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ field, fieldState: { error } }) => (
-        <>
-          {label && (
-            <CustomLabel label={label} error={error} required={required} />
-          )}
-          <DatePicker
-            {...field}
-            {...other}
-            slotProps={{
-              textField: {
-                helperText: (
-                  <Typography
-                    component={"span"}
-                    sx={{ display: "block", mt: -1, ml: -1 }}
-                  >
-                    {error?.message}
-                  </Typography>
-                ),
-                error: error,
-                fullWidth: other.fullWidth,
-                size: other.size,
-                label: "",
-                inputProps: { style: { height: 27 } },
-              },
-            }}
-            label={label}
-          />
-        </>
-      )}
-    />
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field, fieldState: { error } }) => (
+          <>
+            {label && <CustomLabel label={label} error={error} />}
+            <DatePicker
+              {...field}
+              {...other}
+              fullWidth
+              variant={"standard"}
+              slotProps={{
+                textField: {
+                  helperText: (
+                    <Typography
+                      component={"span"}
+                      display={"flex"}
+                      alignItems={"center"}
+                      fontWeight={500}
+                    >
+                      {error?.message && (
+                        <ErrorIcon
+                          sx={{ color: theme?.palette?.error?.main }}
+                        />
+                      )}
+                      {error?.message}
+                    </Typography>
+                  ),
+                  error: error,
+                  fullWidth: other.fullWidth,
+                  size: other.size,
+                  label: "",
+                  inputProps: { style: { height: 27 } },
+                },
+              }}
+              label={label}
+            />
+          </>
+        )}
+      />
+    </LocalizationProvider>
   );
 }
--- src/sections/example-folder/index.tsx
+++ src/sections/example-folder/index.tsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import {
+  RHFDatePicker,
+  RHFSelect,
+  RHFTextField,
+} from "@root/components/react-hook-form";
+import * as Yup from "yup";
 
-export default function ExampleFolder() {
-  return (
-    <div>ExampleFolder</div>
-  )
-}
+export const defaultValues = {
+  textField: "",
+  select: "",
+  mulitSelect: [],
+  datePicker: new Date(),
+};
+
+export const validationSchema = Yup.object().shape({
+  textField: Yup.string().required("Required"),
+  select: Yup.string().required("Required"),
+  mulitSelect: Yup.mixed().required("Required"),
+  datePicker: Yup.date().required("Required"),
+});
+
+export const dataArray = [
+  {
+    id: 1,
+    componentProps: {
+      name: "textField",
+      label: "TextField",
+      placeholder: "TextField",
+    },
+    component: RHFTextField,
+    md: 12,
+  },
+  {
+    id: 2,
+    componentProps: {
+      name: "select",
+      label: "Select",
+      placeholder: "Select",
+      options: ["BE", "BE1", "BE2"],
+    },
+    component: RHFSelect,
+    md: 12,
+  },
+  {
+    id: 3,
+    componentProps: {
+      name: "mulitSelect",
+      label: "MulitSelect",
+      multiple: true,
+      options: ["BE", "BE1", "BE2"],
+    },
+    component: RHFSelect,
+    md: 12,
+  },
+  {
+    id: 4,
+    componentProps: {
+      name: "datePicker",
+      label: "DatePicker",
+    },
+    component: RHFDatePicker,
+    md: 12,
+  },
+];
