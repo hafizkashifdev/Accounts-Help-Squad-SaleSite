9ed3030396b6e13fce27089116dfb50e11a1384b
Nouman Ishtiaq <nouman.ishtiaq@ceative.co.uk>
2023-10-13 20:34:34 +0500

    noman

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
 import ThemeProvider from "@root/theme";
-import { Footer } from "@root/layouts/Footer/Footer";
+import { Footer } from "@root/layouts/footer/Footer";
 import { Header } from "@root/layouts/header/Header";
 import type { Metadata } from "next";
 import { Exo_2 } from "next/font/google";
 import Head from "next/head";
+import Box from "@mui/material/Box";
 
 const inter = Exo_2({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Accounts Help Sqad",
+  title: "Accounts Help Squad",
   description: "Your Trusted Accounting Solutions Partner",
   openGraph: {
-    title: "Accounts Help Sqad",
+    title: "Accounts Help Squad",
     description: "Your Trusted Accounting Solutions Partner",
     url: "https://accountanthelpsquad.co.uk/",
-    siteName: "Accounts Help Sqad",
+    siteName: "Accounts Help Squad",
     images: [
       { url: "../assets/layout/images/og-logo.png", width: 800, height: 600 },
     ],
@@ -38,9 +39,25 @@ export default function RootLayout({
               rel="stylesheet"
             />
           </Head>
-          <Header />
-          {children}
-          <Footer />
+          <Box
+            display={"flex"}
+            flexDirection={"column"}
+            // minHeight={"100vh"}
+          >
+            <Box
+              marginX={{ md: 5 }}
+              // position={"sticky"}
+              // top={0}
+              // zIndex={50}
+              paddingY={2}
+            >
+              <Header />
+            </Box>
+            <Box flexGrow={1}>{children}</Box>
+            <Box>
+              <Footer />
+            </Box>
+          </Box>
         </ThemeProvider>
       </body>
     </html>
