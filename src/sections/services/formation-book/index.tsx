f00d9537098ae9a6f55b8ddca1a59201304b4b15
Ahmer <muhammadahmer.akram@ceative.co.uk>
2023-10-16 17:33:11 +0500

    new page

--- src/sections/services/formation-book/index.tsx
+++ src/sections/services/formation-book/index.tsx
@@ -1,28 +1,33 @@
-"use client";
-
 import { Fragment } from "react";
-import { Grid, Typography, useTheme, Box } from "@mui/material";
+import { Grid, Typography, Box } from "@mui/material";
 import Image from "next/image";
 import FormationBookHero from "@root/assets/services/images/FormatioBookHero.png";
+import CompanyIncorporation from "@root/assets/services/icons/red.svg";
+import BankingServices from "@root/assets/services/icons/blue.svg";
+import LicenseFinder from "@root/assets/services/icons/orange.svg";
+import DocumentServices from "@root/assets/services/icons/purple.svg";
 
 const FormationBook = () => {
-  const theme: any = useTheme();
-
   return (
     <Fragment>
       <Grid container>
-        <Grid item xs={12} md={6} margin={"auto"} paddingX={{ md: 5 }}>
-          <Typography variant="h1" color={theme?.palatte?.primary?.main}>
-            Formation Book
-          </Typography>
-          <Typography variant="h5" color={theme?.palatte?.primary?.main}>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          margin={"auto"}
+          paddingX={{ xs: 1, md: 5 }}
+          color={"primary.main"}
+        >
+          <Typography variant="h1">Formation Book</Typography>
+          <Typography variant="h5">
             Your Gateway to Seamless Business Establishment
           </Typography>
           <Typography
             variant="body1"
             mt={1}
             width={"90%"}
-            color={theme?.palatte?.primary?.lighter}
+            color={"primary.lighter"}
           >
             Welcome to Formation Book, your all-in-one solution for starting and
             managing your business journey. We understand that starting a
@@ -40,22 +45,81 @@ const FormationBook = () => {
           />
         </Grid>
       </Grid>
-      <Box paddingX={{ md: 5 }} textAlign={"center"}>
-        <Typography variant="h2" color={theme?.palatte?.primary?.main}>
+      <Box paddingX={{ xs: 1, md: 5 }} color={"primary.main"}>
+        <Typography variant="h2" textAlign={"center"}>
           Our Comprehensive Services Include
         </Typography>
-        <Grid container spacing={4} textAlign={"unset"}>
-          <Grid item xs={12} md={6}>
-            1
+        <Grid container spacing={4} marginX={"auto"} mt={1}>
+          <Grid item xs={12} md={6} display={"flex"}>
+            <Image
+              src={CompanyIncorporation}
+              width={0}
+              height={0}
+              alt="Company Incorporation"
+            />
+            <Box>
+              <Typography variant="h5">Company Incorporation</Typography>
+              <Typography variant="body1" mt={1} color={"primary.lighter"}>
+                We take care of the mandatory Companies House incorporation
+                process, ensuring your business is set up correctly and legally.
+                Once incorporated, you'll receive a printed Certificate of
+                Incorporation, and we'll provide you with a digital copy for
+                your records.
+              </Typography>
+            </Box>
           </Grid>
-          <Grid item xs={12} md={6}>
-            2
+          <Grid item xs={12} md={6} display={"flex"}>
+            <Image
+              src={BankingServices}
+              width={0}
+              height={0}
+              alt="Banking Services"
+            />
+            <Box>
+              <Typography variant="h5">Banking Services</Typography>
+              <Typography variant="body1" mt={1} color={"primary.lighter"}>
+                Say goodbye to the hassle of searching for a suitable business
+                bank account. With Formation Book, you gain access to a free
+                business bank account, and we sweeten the deal with a merchant
+                account offering cashback benefits. It's banking made easy.
+              </Typography>
+            </Box>
           </Grid>
-          <Grid item xs={12} md={6}>
-            3
+          <Grid item xs={12} md={6} display={"flex"}>
+            <Image
+              src={LicenseFinder}
+              width={0}
+              height={0}
+              alt="License Finder"
+            />
+            <Box>
+              <Typography variant="h5">License Finder</Typography>
+              <Typography variant="body1" mt={1} color={"primary.lighter"}>
+                Not sure which licenses and permits your business needs? Our
+                License Finder tool will help you discover the licenses and
+                permits required for your specific industry and location. We'll
+                even guide you through the process of obtaining theme.
+              </Typography>
+            </Box>
           </Grid>
-          <Grid item xs={12} md={6}>
-            4
+          <Grid item xs={12} md={6} display={"flex"}>
+            <Image
+              src={DocumentServices}
+              width={0}
+              height={0}
+              alt="Document Services"
+            />
+            <Box>
+              <Typography variant="h5">Document Services</Typography>
+              <Typography variant="body1" mt={1} color={"primary.lighter"}>
+                Keeping track of essential company documents is a breeze with
+                Formation Book. We provide printed Share Certificates and
+                Minutes of the First Board Meeting. Plus, all your critical
+                company documents are sent to you by email, including copies of
+                key documents like the Certificate of Incorporation and
+                Memorandum & Articles of Association.
+              </Typography>
+            </Box>
           </Grid>
         </Grid>
       </Box>
