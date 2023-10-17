6ec861b4863f0003b6ff284855bf232a38afc3fc
Rao Mudasir <raomuhammad.mudasir@ceative.co.uk>
2023-10-17 20:49:04 +0500

    faqs completed

---
+++ src/sections/faq/FAQs.data.ts
@@ -0,0 +1,102 @@
+export const faqsTabsData = [
+  "General",
+  "Formation Book",
+  "Accounts Receivable",
+  "Accounts Payable",
+  "CashFlow Management",
+  "R&D Tax Claim",
+];
+export const faqsTabsChildren: {[index: string]:any} = {
+  "General": [
+    {
+      que: "What is Accounts Help Squad?",
+      ans: "Accounts Help Squad is your trusted online platform for a wide range of financial and business services. Learn More",
+    },
+    {
+      que: "How can Accounts Help Squad benefit my business?",
+      ans: "Our services are designed to streamline financial processes, provide expert guidance, and empower your business to make informed decisions. Explore Services",
+    },
+    {
+      que: "How do I get in touch with Accounts Help Squad?",
+      ans: "You can reach out to us through our Contact Us page, where you'll find our contact details for inquiries and support.",
+    },
+    {
+      que: "Is Accounts Help Squad compliant with regulations?",
+      ans: "Yes, we are fully compliant with AML and KYC regulations, ensuring integrity and transparency in our services.",
+    },
+    {
+      que: "What are the fees for using Accounts Help Squad's services?",
+      ans: "Fees vary by service. Please refer to our Pricing Plans page for detailed information on pricing.",
+    },
+  ],
+  "Formation Book": [
+    {
+      que: "How can I incorporate my company through Accounts Help Squad?",
+      ans: "Our Company Incorporation service includes mandatory Companies House incorporation and provides a printed Certificate of Incorporation. Learn More",
+    },
+    {
+      que: "What banking services do you offer with company formation?",
+      ans: "We offer a free business bank account and a merchant account with cashback to streamline your finances. Explore Banking Services",
+    },
+    {
+      que: "Can you help me with annual compliance after incorporation?",
+      ans: "Yes, we offer an Annual Return Filing service to ensure your company remains compliant. View Annual Compliance",
+    },
+  ],
+  "Accounts Receivable": [
+    {
+      que: "How does the Accounts Receivable module work?",
+      ans: "The Accounts Receivable module helps you manage outstanding invoices, aging analysis, and more. Learn More",
+    },
+    {
+      que: "Can I customize invoice templates with this module?",
+      ans: "Yes, you can customize invoice templates with branding and details. Explore Invoice Customization",
+    },
+    {
+      que: "What are the benefits of the client dashboard mentioned in the Accounts Receivable module?",
+      ans: "The client dashboard provides a central hub for managing financial data, reports, and analytics. Discover Client Dashboard",
+    },
+  ],
+  "Accounts Payable": [
+    {
+      que: "How does the Accounts Payable module assist with bill management?",
+      ans: "The Accounts Payable module offers bill management, payment reminders, and more to optimize payment processes. Learn More",
+    },
+    {
+      que: "Can I set up approval workflows for bill payments?",
+      ans: "Yes, you can set up approval workflows for timely bill payments. Explore Payment Approval",
+    },
+    {
+      que: "How does the module help with cash flow projections?",
+      ans: "The module provides cash flow projections based on payables to help you plan finances. Discover Cash Flow Projections",
+    },
+  ],
+  "CashFlow Management": [
+    {
+      que: "What insights can I gain from the CashFlow Management module?",
+      ans: "The module allows you to automate cash flow tracking and forecasting. View Automation Features",
+    },
+    {
+      que: "How does it handle currency conversion for cash flow?",
+      ans: "The module guides you through the process of claiming R&D tax relief and dealing with HMRC. Learn More",
+    },
+    {
+      que: "Can I automate cash flow tracking and reporting?",
+      ans: "The module explains various types of R&D tax relief and eligibility criteria. Explore Tax Relief",
+    },
+  ],
+  "R&D Tax Claim": [
+    {
+      que: "How can I benefit from the R&D Tax Claim module?",
+      ans: "The module guides you through the process of claiming R&D tax relief and dealing with HMRC. Learn More",
+    },
+    {
+      que: "What types of R&D tax relief are available?",
+      ans: "The module explains various types of R&D tax relief and eligibility criteria. Explore Tax Relief",
+    },
+    {
+      que: "What is the estimated timeline for receiving benefits after submission?",
+      ans: "The module outlines the estimated timeline for the approval and benefit receipt process. Discover Timeline",
+    },
+  ],
+};
--- src/sections/faq/index.tsx
+++ src/sections/faq/index.tsx
@@ -1,38 +1,50 @@
-import { Grid, Typography } from "@mui/material";
+"use client";
+import { Box, Grid, Typography } from "@mui/material";
 import { getStartedStyles } from "../get-started/GetStarted.styles";
 import HorizontalTabs from "@root/components/tabs/horizontal-tabs/HorizontalTabs";
-import HorizontalTabsExample from "@root/components/tabs/horizontal-tabs/HorizontalTabs.example";
+import { faqsTabsChildren, faqsTabsData } from "./FAQs.data";
+import { useState } from "react";
+import { FaqsItem } from "@root/components/faqs-item";
+import Searchbar from "@root/components/searchbar";
 
 const FAQs = () => {
-  return <>
-    <Grid container>
-      <Grid item xs={12} sx={getStartedStyles.heroContainer}>
-        <Typography variant="h1" color="common.main">
-          Let’s Get Started
-        </Typography>
-        <Typography variant="h6" color="common.light" maxWidth="799px">
-          Welcome to Accounts Help Squad, your gateway to seamless
-          communication. Whether you have questions, feedback, or simply want
-          to get in touch, this is the place to do it. We're eager to hear
-          from you and provide the information and assistance you need. Feel
-          free to reach out, and we'll respond promptly to assist you in any
-          way we can.
-        </Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <Grid container sx={{ justifyContent: "center" }}>
-          <Grid item md={10} xs={11}>
-            <HorizontalTabs tabsDataArray={['General',
-              'Formation Book',
-              'Accounts Receivable',
-              'Accounts Payable',
-              'CashFlow Management',
-              'R&D Tax Claim']} />
+  const [activeTab, setActiveTab] = useState<any>("General");
+
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={12} sx={getStartedStyles.heroContainer}>
+          <Typography variant="h1" color="common.main">
+            Let’s Get Started
+          </Typography>
+          <Typography variant="h6" color="common.light" maxWidth="799px">
+            Welcome to Accounts Help Squad, your gateway to seamless
+            communication. Whether you have questions, feedback, or simply want
+            to get in touch, this is the place to do it. We're eager to hear
+            from you and provide the information and assistance you need. Feel
+            free to reach out, and we'll respond promptly to assist you in any
+            way we can.
+          </Typography>
+          <Box sx={{mt:'30px'}}>
+          <Searchbar />
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container sx={{ justifyContent: "center" }}>
+            <Grid item md={10} xs={11}>
+              <HorizontalTabs
+                tabsDataArray={faqsTabsData}
+                setActiveTab={setActiveTab}
+              />
+              {faqsTabsChildren[activeTab].map((item: any) => (
+                <FaqsItem title={item.que} description={item.ans} />
+              ))}
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
-  </>;
+    </>
+  );
 };
 
 export default FAQs;
