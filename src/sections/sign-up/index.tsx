c94466277cdfaba44fcabd48a62337b0c3a80fd5
Ahmer <muhammadahmer.akram@ceative.co.uk>
2023-10-18 17:29:58 +0500

    signup form fixings

--- src/sections/sign-up/index.tsx
+++ src/sections/sign-up/index.tsx
@@ -2,48 +2,21 @@ import React from "react";
 import { Container, Grid, Typography } from "@mui/material";
 import { SignUpForm } from "./sign-up-form/SignUpForm";
 
-const formContainerStyle = {
-  background: "white",
-  padding: "20px",
-  borderRadius: "16px",
-  boxShadow: "1px solid rgba(204, 204, 204, 0.50);",
-  height: "668px",
-  border: "1px solid rgba(204, 204, 204, 0.50);",
-};
-
 const SignUp = () => {
   return (
-    <Container maxWidth="lg" style={formContainerStyle}>
-      <Grid
-        container
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Grid item>
-          <Typography
-            sx={{
-              whiteSpace: {
-                xs: "wrap",
-                sm: "wrap",
-                md: "nowrap",
-                lg: "nowrap",
-              },
-              fontSize: "32px",
-              fontWeight: 600,
-              fontStyle: "normal",
-              color: "#666",
-            }}
-          >
-            Register Yourself
-          </Typography>
-        </Grid>
-        <Grid item>
-          <SignUpForm />
-        </Grid>
-      </Grid>
+    <Container
+      maxWidth="lg"
+      sx={{
+        backgroundColor: "common.white",
+        padding: 3,
+        borderRadius: 4,
+        boxShadow: 4,
+      }}
+    >
+      <Typography variant="h3" textAlign={"center"} color={"primary.lighter"}>
+        Register Yourself
+      </Typography>
+      <SignUpForm />
     </Container>
   );
 };
--- src/sections/sign-up/sign-up-form/SignUpForm.tsx
+++ src/sections/sign-up/sign-up-form/SignUpForm.tsx
@@ -1,61 +1,47 @@
 "use client";
+
 import { Box, Grid } from "@mui/material";
 import { FormProvider } from "@root/components/react-hook-form";
-import { useSignUpForm } from "./useSignUpForm";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { defaultValues, dataArray, validationSchema } from "./SignUpForm.data";
 import { LoadingButton } from "@mui/lab";
 
 export const SignUpForm = () => {
-  const {
-    methods,
-    handleSubmit,
-    submitRequestADemoForm,
-    requestADemoFormFieldsData,
-  } = useSignUpForm();
+  const methods: any = useForm({
+    resolver: yupResolver(validationSchema),
+    defaultValues,
+  });
+
+  const { handleSubmit } = methods;
+
+  const onSubmit = async (data: any) => {
+    console.log(data);
+  };
+
   return (
     <Box sx={{ py: "30px" }}>
-      <FormProvider
-        methods={methods}
-        onSubmit={handleSubmit(submitRequestADemoForm())}
-      >
-        <Grid container spacing={3}>
-          {requestADemoFormFieldsData?.map((form: any) => {
-            return (
-              <>
-                <Grid item xs={12} lg={6} md={form?.gridLength} key={form?.id}>
-                  <form.component
-                    {...form.componentProps}
-                    size="small"
-                    onSubmit={handleSubmit(submitRequestADemoForm())}
-                  >
-                    {form?.componentProps?.select
-                      ? form?.componentProps?.options?.map((option: any) => (
-                          <option key={option?.id} value={option?.value}>
-                            {option?.label}
-                          </option>
-                        ))
-                      : form?.heading
-                      ? form?.heading
-                      : null}
-                  </form.component>
-                </Grid>
-              </>
-            );
-          })}
-          <LoadingButton
-            fullWidth
-            sx={{
-              marginTop: 8,
-              marginLeft: 2,
-              padding: "16px 32px",
-              borderRadius: "8px",
-            }}
-            type="submit"
-            variant="contained"
-            onClick={handleSubmit(submitRequestADemoForm())}
-          >
-            Register Yourself
-          </LoadingButton>
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={4}>
+          {dataArray?.map((item: any) => (
+            <Grid item xs={12} md={item?.md} key={item?.id}>
+              <item.component {...item.componentProps} />
+            </Grid>
+          ))}
         </Grid>
+        <LoadingButton
+          fullWidth
+          sx={{
+            padding: "16px 32px",
+            borderRadius: "8px",
+            marginTop: 2,
+            backgroundColor: "primary.lighter",
+          }}
+          type="submit"
+          variant="contained"
+        >
+          Register Yourself
+        </LoadingButton>
       </FormProvider>
     </Box>
   );
